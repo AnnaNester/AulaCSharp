19c51d29fafd2de1c009710550d1d4fac94c9d08
Anna <anna.nester@gmail.com>
2018-05-10 09:16:54 -0300

    Versão inicial do projeto POO (Conceitual)

---
+++ Aulas/Aula0705_Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Aula0705_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine("Hello World!");
+
+
+        }
+    }
+}
