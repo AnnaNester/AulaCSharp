fadd9ecb13ef84e7962f35dd9ab45686b33eee21
Anna <anna.nester@gmail.com>
2018-05-10 10:34:01 -0300

    Migrando informações de Cliente para Pessoa

--- Aulas/Aula1005_POO/Cliente.cs
+++ Aulas/Aula1005_POO/Cliente.cs
@@ -2,40 +2,7 @@
 {
     class Cliente
     {
-        //Atributo
-        private string nome;
-
-        //Propriedade
-        public string Nome
-        {
-            get
-            {
-                return nome;
-            }
-
-            set
-            {
-                nome = value;
-            }
-        }
-
-        public string Cpf { get; set; }
-
-        //Construtor
-        public Cliente() //Construtor vazio
-        {
-
-        }
-
-        public Cliente (string nome)
-        {
-            Nome = nome;
-        }
-
-        public Cliente (string nome, string cpf)
-        {
-            Nome = nome;
-            Cpf = cpf;
-        }
+       
+ 
     }
 }
