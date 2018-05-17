dd8915de0707e3cee6b7057612f51a2eb1c4c0ed
Anna <anna.nester@gmail.com>
2018-05-17 08:44:28 -0300

    Criando a classe AtividadesController

---
+++ Aulas/Aula1705_Camadas/Controllers/AtividadesController.cs
@@ -0,0 +1,16 @@
+﻿namespace Aula1705_Camadas.Controllers
+{
+    class AtividadesController
+    {
+        //simular tabela
+        public List<Atividade> ListaAtividades { get; set; }
+
+
+        //Salvar
+        //Listar
+        //Ler
+        //Editar
+        //Excluir
+
+    }
+}
